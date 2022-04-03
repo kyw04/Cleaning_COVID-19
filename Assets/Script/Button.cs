9cdc3a60243fbb4424d78e5f8930d6e128509d8e
kyw04 <85551017+kyw04@users.noreply.github.com>
2022-04-03 18:13:05 +0900

    메뉴 수정

--- Assets/Script/Button.cs
+++ Assets/Script/Button.cs
@@ -2,11 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class Button : MonoBehaviour
 {
+    public Image image;
+    private bool starting = false;
+
+    private void Start()
+    {
+        StartCoroutine(start());
+    }
+    void Update()
+    {
+        if (starting)
+            transform.position += transform.up * 2 * Time.deltaTime;
+    }
+
     public void start_click()
     {
+        GameObject button =  EventSystem.current.currentSelectedGameObject;
+        button.SetActive(false);
+        StartCoroutine(change_scene());
+    }
+
+    IEnumerator change_scene()
+    {
+        starting = true;
+        while (image.color.a <= 1)
+        {
+            yield return new WaitForSeconds(0.001f);
+            image.color += new Color32(0, 0, 0, 10);
+        }
+
         SceneManager.LoadScene(1);
+    }
+    IEnumerator start()
+    {
+        starting = true;
+        yield return new WaitForSeconds(0.75f);
+        starting = false;
     }
 }
